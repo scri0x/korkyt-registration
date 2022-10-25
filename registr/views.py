c6a64f5832b79d4d47f2afa7340ff5886db97e88
Vladimir <1warmn@bk.ru>
2022-10-25 18:15:20 +0500

    added index, email, edu, course, last html files in views

--- registr/views.py
+++ registr/views.py
@@ -1,3 +1,30 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.template import loader
 
-# Create your views here.
+
+def index(request):
+    temp = loader.get_template('index.html')
+    return HttpResponse(temp.render())
+
+
+def email(request):
+    temp = loader.get_template('email.html')
+    return HttpResponse(temp.render())
+
+
+def edu(request):
+    temp = loader.get_template('edu.html')
+    return HttpResponse(temp.render())
+
+
+def course(request):
+    temp = loader.get_template('course.html')
+    return HttpResponse(temp.render())
+
+
+def last(request):
+    temp = loader.get_template('last.html')
+    return HttpResponse(temp.render())
+
+
